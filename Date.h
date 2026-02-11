8d7618f9498780a25bf8ecba33e68362ba5136ed
onerealviana <vianmebrahimi@gmail.com>
2026-02-11 14:34:48 +0330

    Update Date.h

--- Date.h
+++ Date.h
@@ -10,3 +10,21 @@ struct Date {
     int year;
     int month;
     int day;
+    Date(int y = 1402, int m = 1, int d = 1) : year(y), month(m), day(d) {}
+    
+    string toString() const {
+        return to_string(year) + "/" + to_string(month) + "/" + to_string(day);
+    }
+    
+    string serialize() const {
+        return to_string(year) + " " + to_string(month) + " " + to_string(day);
+    }
+    
+    static Date deserialize(const string& str) {
+        int y, m, d;
+        sscanf(str.c_str(), "%d %d %d", &y, &m, &d);
+        return Date(y, m, d);
+    }
+};
+
+#endif
