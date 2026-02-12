4474f694a2927e4695ed8515aa639e8a1c74d7be
onerealviana <vianmebrahimi@gmail.com>
2026-02-12 18:23:40 +0330

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "LinkoNetwork.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+void printMenu() {
+    cout << "\n=============== Linko Social Network ===============" << endl;
+    cout << "Available commands:" << endl;
+    cout << "  register <username> <password> \"<display name>\" \"<bio>\"" << endl;
+    cout << "  login <username> <password>" << endl;
+    cout << "  logout" << endl;
+    cout << "  post \"<content>\"" << endl;
+    cout << "  feed [time|like]" << endl;
+    cout << "  follow <username>" << endl;
+    cout << "  unfollow <username>" << endl;
+    cout << "  like <post_id>" << endl;
+    cout << "  comment <post_id> \"<text>\"" << endl;
+    cout << "  delete <post_id>" << endl;
+    cout << "  profile \"<display name>\" \"<bio>\"" << endl;
+    cout << "  show [username]" << endl;
+    cout << "  save" << endl;
+    cout << "  exit" << endl;
+    cout << "  help" << endl;
+    cout << "==================================================\n" << endl;
+}
